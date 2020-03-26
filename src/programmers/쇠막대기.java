f59e06935366e0e67c5d44984a93787b9461e795
ryunian <kls6221@naver.com>
2020-03-27 06:43:35 +0900

    3/27

---
+++ src/programmers/쇠막대기.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.Stack;
+
+public class 쇠막대기 {
+    public int solution(String arrangement) {
+        int answer = 0;
+        String[] arr = arrangement.split("");
+        Stack<String> stack = new Stack<>();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].equals("(")){
+                stack.add(arr[i]);
+            }else if(arr[i].equals(")") && arr[i-1].equals("(")){
+                stack.pop();
+                answer += stack.size();
+//                System.out.println(stack.size());
+            }else if(arr[i].equals(")") && arr[i-1].equals(")")){
+                stack.pop();
+                answer++;
+//                System.out.println(1);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String arrangement = "()(((()())(())()))(())";
+
+        쇠막대기 main = new 쇠막대기();
+        int result = main.solution(arrangement);
+        System.out.println(result); // 17
+    }
+}
