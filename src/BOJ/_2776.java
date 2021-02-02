9480e6f04113f9845239c460cfbb7ed4e04d4d30
ryunian <kls6221@naver.com>
2021-02-02 23:47:11 +0900

    2021/02/02 BOJ

---
+++ src/BOJ/_2776.java
@@ -0,0 +1,55 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _2776 {
+    static int n;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+
+        while (test-- > 0) {
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+            int m = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                solve(num);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void solve(int target) {
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                sb.append(1).append("\n");
+                return;
+            }
+        }
+        sb.append(0).append("\n");
+    }
+}
+
