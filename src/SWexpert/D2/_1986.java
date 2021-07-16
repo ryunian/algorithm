e06718c3a7ad5d61a1bebf36e6954144e2b54755
ryunian <51253582+ryunian@users.noreply.github.com>
2021-07-16 19:44:35 +0900

    SWexpert

---
+++ src/SWexpert/D2/_1986.java
@@ -0,0 +1,19 @@
+package SWexpert.D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            // n 이 짝수 일때, n/2 * -1
+            // n 이 홀수 일떄, (n+1) / 2
+            int res = n % 2 == 0 ? (n / 2) * -1 : (n + 1) / 2;
+            System.out.println("#" + i + " " + res);
+        }
+    }
+}
