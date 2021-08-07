868e54ecff2f03870b31a4de8d7875931219f949
ryunian <51253582+ryunian@users.noreply.github.com>
2021-08-08 00:25:44 +0900

    SWexpert

---
+++ src/SWexpert/_3499.java
@@ -0,0 +1,37 @@
+package SWexpert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class _3499 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+
+        List<String> list;
+        StringBuilder sb = new StringBuilder();
+
+        for (int tc = 1; tc <= test; tc++) {
+            int n = sc.nextInt();
+
+            list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                list.add(sc.next());
+            }
+
+            int div = (n + 1) / 2;
+            int x = 0;
+            int y = div;
+
+            sb.append("#" + tc + " ");
+            for (int i = y; i < n; i++) {
+                sb.append(list.get(x++) + " ");
+                sb.append(list.get(y++) + " ");
+            }
+            if (n % 2 == 1) sb.append(list.get(div - 1));
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
