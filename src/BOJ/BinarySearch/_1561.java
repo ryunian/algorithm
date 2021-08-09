fe74bf2b1a4828ea22f7a6f1e81beab5ab580996
ryunian <51253582+ryunian@users.noreply.github.com>
2021-08-09 21:53:45 +0900

    add comment

--- src/BOJ/BinarySearch/_1561.java
+++ src/BOJ/BinarySearch/_1561.java
@@ -14,23 +14,26 @@ public class _1561 {
         int m = Integer.parseInt(st.nextToken());
         int[] arr = new int[m];
 
-        st = new StringTokenizer(br.readLine());
+//        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = 30;
+//            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         long left = 0;
         long right = 2000000000L * 30; // n의 최대값과 운행시간의 곱
-        long sum = 0;
-        long cnt = 0;
+        long sum = 0; // 놀이기구를 탄 사람 수
+        long cnt = 0; // n 번째 탄 사람의 시간
         while (left < right) {
             long mid = (left + right) / 2;
             long tmp = 0;
+            // tmp : mid 일떄 놀이기구를 탄 수
             for (int i = 0; i < m; i++) {
-                tmp += (mid / arr[i]) + 1;
+                tmp += (mid / arr[i]) + 1; // 0 초일떄는 전부 다 탈수 있으므로 +1 한다.
             }
+            System.out.println(left+" "+mid+" "+right);
             // 같은 tmp가 나올수가 있으므로
-            // tmp가 같을경우 가장 작은 cnt를 찾는다 (lowerBound)
+            // tmp가 같을경우 가장 작은 cnt 를 찾는다 (lowerBound)
             if (tmp < n) {
                 sum = tmp;
                 cnt = mid + 1;
@@ -43,8 +46,10 @@ public class _1561 {
                 right = mid;
             }
         }
+        System.out.println(sum + " " + cnt);
+        // 놀이기구를 탄 사람 수랑 같으므로
+        // 뒤에서부터 현 시간에 탑승 가능한 놀이기구 탐색
         if (n == sum) {
-            // 뒤에서 부터 탐색
             for (int i = m - 1; i >= 0; i--) {
                 if (cnt % arr[i] == 0) {
                     System.out.println(i + 1);
@@ -53,6 +58,7 @@ public class _1561 {
             }
         }
         // 앞에서 부터 탐색
+        // 탑승가능한 놀이기구일때 탑승인원을 늘리면서 n 값이랑 같으면 종료
         for (int i = 0; i < m; i++) {
             if (cnt % arr[i] == 0) {
                 sum++;
