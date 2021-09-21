857a517c257ebb811cb49266a8f605c6f6a69e13
Ryunian <kls6221@naver.com>
2021-09-22 00:09:38 +0900

    Refactoring

--- src/BOJ/Sort/_11004.java
+++ src/BOJ/Sort/_11004.java
@@ -3,66 +3,24 @@ package BOJ.Sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class _11004 {
-    static int n, k;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list = new ArrayList<>(n);
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        while (n-- > 0){
+            list.add(Integer.parseInt(st.nextToken()));
         }
-        quickSort(arr, 0, n - 1);
-        System.out.println(arr[k - 1]);
-    }
-
-    public static void quickSort(int[] arr, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        // 분할
-        int q = partition(arr, left, right);
-
-        if (q + 1 < k) {
-            // 오른쪽부분 정렬
-            quickSort(arr, q + 1, right);
-        } else if (q + 1 > k) {
-            // 왼쪽부분 정렬
-            quickSort(arr, left, q - 1);
-        } else {
-            return;
-        }
-
-    }
-
-    private static int partition(int[] arr, int left, int right) {
-        int mid = (left + right) / 2;
-        swap(arr, left, mid);
-        int pivot = arr[left];
-        int i = left, j = right;
-        while (i < j) {
-            while (pivot < arr[j]) {
-                j--;
-            }
-            while (i < j && pivot >= arr[i]) {
-                i++;
-            }
-            swap(arr, i, j);
-        }
-        arr[left] = arr[i];
-        arr[i] = pivot;
-        return i;
-    }
-
-    private static void swap(int[] arr, int a, int b) {
-        int tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
+        Collections.sort(list);
+        System.out.println(list.get(k-1));
     }
 }
